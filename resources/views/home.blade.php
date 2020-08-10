113aba279fd3e21529eba8d2209bf85fec5e7de0
omojolasamson <sammy_omojola@yahoo.com>
2020-08-10 02:00:46 +0100

    left a message on home profile page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('This is where images shoul be displayed') }}
                 </div>
             </div>
         </div>
